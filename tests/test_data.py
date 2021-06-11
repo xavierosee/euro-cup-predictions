f3a2c09c081356b418ee54fbde32e09572339d53
Xavier Rosee <xrosee@live.cn>
2021-06-11 12:55:01 +0200

    ✅ add tests for UEFA ranking imports

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,9 +3,14 @@ from euro_cup_predictions import data
 import pandas as pd
 
 fifa21_rankings = data.import_from_ea()
+uefa_rankings = data.import_from_uefa()
 
 
 class TestImports(unittest.TestCase):
     def test_fifa_21_rankings(self):
         self.assertIsInstance(fifa21_rankings, pd.DataFrame)
         self.assertEqual(fifa21_rankings.shape[1], 80)
+
+    def test_uefa_rankings(self):
+        self.assertIsInstance(uefa_rankings, pd.DataFrame)
+        self.assertEqual(uefa_rankings.shape[1], 26)
